bced8cd4cea3da13e3e457f25dc98f21776ae8e4
altunsercan <altunsercan@users.noreply.github.com>
2020-05-16 16:36:52 +0000

    deploy: 194d3c21215a284c6fa57333d19084b3d719e9f6

---
+++ src/components/Nav.vue
@@ -0,0 +1,41 @@
+<template>
+  <nav id="patterns-nav" class="patterns" role="navigation">
+    <h2 class="vh">Main navigation</h2>
+    <button id="menu-button" aria-expanded="false">
+      <svg viewBox="0 0 50 50" aria-hidden="true" focusable="false">
+        <use xlink:href="#menu"></use>
+      </svg>
+      Menu
+    </button>
+
+    <ul id="patterns-list">
+      <li class="pattern">
+        <g-link class="nav__link" to="/">
+          <svg class="bookmark-icon" aria-hidden="true" focusable="false" viewBox="0 0 40 50">
+            <use xlink:href="#bookmark"></use>
+          </svg>
+          <span class="text">Home</span>
+        </g-link>
+      </li>
+
+      <li class="pattern">
+        <g-link class="nav__link" to="/about">
+          <svg class="bookmark-icon" aria-hidden="true" focusable="false" viewBox="0 0 40 50">
+            <use xlink:href="#bookmark"></use>
+          </svg>
+          <span class="text">About</span>
+        </g-link>
+      </li>
+    </ul>
+  </nav>
+</template>
+
+<script>
+  export default {
+    name: "Nav"
+  }
+</script>
+
+<style scoped>
+
+</style>
---
+++ src/pages/Index.vue
@@ -0,0 +1,23 @@
+<template>
+  <Layout>
+
+    <main id="main">
+      <h1>Oyun Basını Rehberi</h1>
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Pariatur excepturi labore tempore expedita, et iste tenetur suscipit explicabo! Dolores, aperiam non officia eos quod asperiores
+      </p>
+    </main>
+  </Layout>
+</template>
+
+<script>
+export default {
+  metaInfo: {
+    title: 'Oyun Basını Rehberi'
+  }
+}
+</script>
+
+<style>
+
+</style>
