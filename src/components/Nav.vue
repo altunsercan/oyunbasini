61a3e2f9e64da57bb45b8142fafb75d65eed412d
Sercan Altun <altun.sercan@gmail.com>
2020-05-16 22:52:25 -0400

    Update nav to get selected on current page

--- src/components/Nav.vue
+++ src/components/Nav.vue
@@ -9,24 +9,40 @@
     </button>
 
     <ul id="patterns-list">
-      <li class="pattern">
-        <g-link class="nav__link" to="/">
+      <li class="pattern" >
+        <g-link class="nav__link" v-bind:aria-current="$route.path === '/'" to="/">
           <svg class="bookmark-icon" aria-hidden="true" focusable="false" viewBox="0 0 40 50">
-            <use xlink:href="#bookmark"></use>
+            <use xlink:href="#bookmark">
+              <symbol id="bookmark" viewBox="0 0 40 50">
+                <g transform="translate(2266 3206.2)">
+                  <path style="stroke:currentColor;stroke-width:3.2637;fill:none" d="m-2262.2-3203.4-.2331 42.195 16.319-16.318 16.318 16.318.2331-42.428z"></path>
+                </g>
+              </symbol>
+            </use>
           </svg>
           <span class="text">Home</span>
         </g-link>
       </li>
 
       <li class="pattern">
-        <g-link class="nav__link" to="/about">
+        <g-link class="nav__link"  v-bind:aria-current="$route.path === '/about'"  to="/about">
           <svg class="bookmark-icon" aria-hidden="true" focusable="false" viewBox="0 0 40 50">
-            <use xlink:href="#bookmark"></use>
+            <use xlink:href="#bookmark">
+              <symbol id="bookmark" viewBox="0 0 40 50">
+                <g transform="translate(2266 3206.2)">
+                  <path style="stroke:currentColor;stroke-width:3.2637;fill:none" d="m-2262.2-3203.4-.2331 42.195 16.319-16.318 16.318 16.318.2331-42.428z"></path>
+                </g>
+              </symbol>
+            </use>
           </svg>
           <span class="text">About</span>
         </g-link>
       </li>
     </ul>
+
+    <div class="nav-bottom">
+      Project by <g-link href="http://www.unog.dev">ÜNOG</g-link>
+    </div>
   </nav>
 </template>
 
@@ -37,5 +53,11 @@
 </script>
 
 <style scoped>
+.nav-bottom
+{
+  position: absolute;
+  margin-bottom: 1rem;
+  bottom: 0px;
+}
 
 </style>
