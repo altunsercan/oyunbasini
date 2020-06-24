4419340a308a9a492bba6b2ecb79d9f05395fe5a
Sercan Altun <altun.sercan@gmail.com>
2020-06-24 18:24:22 -0400

    Update index page

--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -4,8 +4,24 @@
     <main id="main">
       <h1>Oyun Basını Rehberi</h1>
       <p>
-
+        Türk oyun geliştirme komunitesi oyun geliştirme araçlarına erişimin kolaylaşması ile dünya çapında genişleyen
+        oyun sektörünün bir parçası olarak her geçen gün büyümeye devam ediyor. Hobi veya profesyönel, bağımsız veya kurumsal
+        Türkiye'deki ekiplerce gelişirilen pek çok oyun projesi global pazarda rakabete hazırlanıyor. Ancak Türk oyun geliştiricileri
+        ile yerel ve global oyun basın arasında sağlıklı komunikasyon oluşturabilmek sektörün önündeki önemli bariyerlerden biri olarak öne çıkıyor.
       </p>
+      <p>
+        Bu web projesi ile hedefimiz, Türkiye'deki oyun geliştirme komuniteleri ve ekiplerinin basın ile iletişim hakkındaki
+        bilgi, tecrübe ve kaynağa erişimini kolaylaştırmak. Derlediğimiz rehberler ile hem yerel oyun geliştiricilerine hem de
+        yerel oyun basınına fayda sağlayacağımıza inanıyoruz.
+      </p>
+
+      <h2>Rehberler ve Projeler</h2>
+      <ul>
+        <li>Press Kit/Basın Kiti Nedir ve Nasıl Hazırlanır</li>
+        <li>Github ile Press Kit Websayfası Yayınlama Rehberi <i>Yapım Aşamasında</i></li>
+        <li>Basın Bülteni Nedir ve Nasıl Hazırlanır <i>Yapım Aşamasında</i></li>
+        <li>CD-Key Dağıtım Yöntemleri ve Dikkat Edilmesi Gerekenler <i>Yapım Aşamasında</i></li>
+      </ul>
     </main>
   </Layout>
 </template>
